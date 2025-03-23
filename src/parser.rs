42890a684dd186515ff1132912b5e4c342237873
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2025-03-22 21:22:49 -0700

    fix: remove debug comments
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- src/parser.rs
+++ src/parser.rs
@@ -19,56 +19,6 @@ pub enum Operation {
     Dismiss = 3,
 }
 
-// char identifier [0;4];
-// int size;
-// float speed_Kmh;
-// float speed_Mph;
-// float speed_Ms;
-//
-// bool isAbsEnabled;
-// bool isAbsInAction;
-// bool isTcInAction;
-// bool isTcEnabled;
-// bool isInPit;
-// bool isEngineLimiterOn;
-//
-//
-// float accG_vertical;
-// float accG_horizontal;
-// float accG_frontal;
-//
-// int lapTime;
-// int lastLap;
-// int bestLap;
-// int lapCount;
-//
-// float gas [56-60];
-// float brake;
-// float clutch;
-// float engineRPM;
-// float steer;
-// int gear;
-// float cgHeight;
-//
-// float wheelAngularSpeed[4];
-// float slipAngle[4];
-// float slipAngle_ContactPatch[4];
-// float slipRatio[4];
-// float tyreSlip[4];
-// float ndSlip[4];
-// float load[4];
-// float Dy[4];
-// float Mz[4];
-// float tyreDirtyLevel[4];
-//
-// float camberRAD[4];
-// float tyreRadius[4];
-// float tyreLoadedRadius[4];
-// float suspensionHeight[4];
-// float carPositionNormalized;
-// float carSlope;
-// float carCoordinates[3];
-//
 // the kind of message we can receive from the UDP server
 // reference for parsing: https://docs.google.com/spreadsheets/d/1PhWgG1B7cv38OEummTZOOItrE-yYRBpMI2nV92BfDFU/pubhtml?gid=0&single=true
 #[derive(Debug)]
